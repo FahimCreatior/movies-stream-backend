69e0c090ce29e897787367c8f00b86a9e4405fe9
FahimCreatior <153365037+FahimCreatior@users.noreply.github.com>
2026-03-31 06:21:17 -0600

    Add refreshTokensHandler to user routes

--- routes/user.ts
+++ routes/user.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { signInHandler, signOutHandler, signUpHandler, userDeactivateHandler, userQueryHandler, userUpdateHandler } from '../controllers/user.controller.js';
+import { signInHandler, signOutHandler, signUpHandler, userDeactivateHandler, userQueryHandler, userUpdateHandler, refreshTokensHandler } from '../controllers/user.controller.js';
 import { requireUser } from '../middlewares/requireUser.js';
 import { validateRequest } from '../middlewares/validateRequest.js';
 import { UserSchema, UserSignInSchema, UserUpdateSchema } from '../schemas/user.schema.js';
@@ -12,6 +12,7 @@ router.route('/').get(requireUser, userQueryHandler)
 
 router.post('/SignUp', validateRequest(UserSchema), signUpHandler)
 router.post('/SignIn', validateRequest(UserSignInSchema), signInHandler)
+router.post('/refresh', refreshTokensHandler)
 router.get('/SignOut', requireUser, signOutHandler)
 
 router.delete('/', requireUser, userDeactivateHandler)
